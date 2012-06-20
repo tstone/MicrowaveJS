4df731a7165eb0b1f74814353533471127929111
Grant Klinsing <grant@grantrklinsing.com>
2012-06-20 11:56:09 -0600

    fixes #44

--- src/lib.js
+++ src/lib.js
@@ -1,7 +1,7 @@
 
 // slugify
 exports.slugify = function(s) {
-    return s ? s.trim().toLowerCase().replace(/[^-a-z0-9,&\s]+/ig, '').replace(/\s/g, '-') : '';
+    return s ? s.trim().toLowerCase().replace(/[^-a-z0-9,\s]+/ig, '').replace(/[&\s]+/g, '-') : '';
 };
 
 // titleize
