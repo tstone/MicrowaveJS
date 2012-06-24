1b4664c63452c74742922d2411283fd7da1da28f
Titus <wastingtape@gmail.com>
2012-06-24 10:15:04 -0600

    Theme assets now get bundled too

--- src/assets.js
+++ src/assets.js
@@ -8,11 +8,11 @@ module.exports = function(assets) {
 
     // CSS
     assets.addCss('base.css');
-    assets.addCss('theme/theme.css');
-    assets.addCss('theme/prettify-theme.css');
+    assets.addCss('theme/**');
 
     // JS
     assets.addJs('prettify.js');
     assets.addJs('js/site.js');
+    assets.addCss('theme/**');
 
 };
